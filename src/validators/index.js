1a563ad9d4486e84bb6c595cb2feb139875026ad
Andrew Wittrock <andy@wittrock.us>
2017-09-12 15:51:16 -0500

    Add additional standard validators

--- src/validators/index.js
+++ src/validators/index.js
@@ -10,8 +10,13 @@ export default {
         name + ' must be at least ' + len + ' characters',
         400
     ),
+    matches: (val, pattern, msg) => validate(
+        pattern.test(val),
+        msg,
+        400
+    ),
     isAlphaNumeric: (val, name) => validate(
-        ["string", "number"].includes(typeof val) && /^[a-zA-Z0-9 ]*$/.test(val),
+        ["string", "number"].includes(typeof val) && /^[a-zA-Z0-9]*$/.test(val),
         name + ' must contain only letters and numbers',
         400
     ),
@@ -24,5 +29,21 @@ export default {
         val !== null && val !== "" && typeof val !== 'undefined',
         name + ' is required',
         400
-    )
+    ),
+    missing: (val, name) => validate(
+        typeof val === 'undefined',
+        name + " should not be set",
+        400
+    ),
+    greaterThan: (val, name, threshold) => validate(
+        val > threshold,
+        name + " must be greater than " + threshold,
+        400
+    ),
+    lessThan: (val, name, threshold) => validate(
+        val < threshold,
+        name + " must be less than " + threshold,
+        400
+    ),
+    pass: () => validate(true, "", 200)
 }
